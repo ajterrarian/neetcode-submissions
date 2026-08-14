9ff1a961fe19c6e88d9af2b3298c74241143b0ae
Aarav Jamdar <199998374+ajterrarian@users.noreply.github.com>
2026-08-13 22:22:57 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #strip, lowercase, remove punctuation
+        s = ''.join(ch.lower() for ch in s if ch.isalnum())
+
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            if s[start] != s[end]:
+                return False
+
+            start += 1
+            end -= 1
+            
+        return True
